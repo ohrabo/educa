496ca78d0025ff2152a71425fb1003c62145ad0b
ohrabo01 <ohrabo01@mail.bbk.ac.uk>
2018-07-23 19:56:55 +0100

    log-in, create course works; delete course has 'QuerySet' object is not callable error

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,54 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from .models import Course
 
-# Create your views here.
+
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Course
+
+
+class OwnerMixin(object):
+    @property
+    def get_queryset(self):
+        qs = super(OwnerMixin, self).get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin(object):
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(OwnerEditMixin, self).form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+class OwnerCourseEditMixin(OwnerCourseMixin):
+    fields = ['subject', 'title', 'slug', 'overview']
+    template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+
+    def get_queryset(self):
+        qs = super(ManageCourseListView, self).get_queryset
+        return qs.filter(owner=self.request.user)
+
+class CourseCreateView(OwnerCourseEditMixin, OwnerEditMixin, CreateView, PermissionRequiredMixin):
+    template_name = 'courses/manage/course/form.html'
+    permission_required = 'courses.can_add'
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    template_name = 'courses/manage/course/form.html'
+    permission_required = 'courses.change_course'
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+    success_url = reverse_lazy('manage_course_list')
+    permission_required = 'courses.delete_course'
--- educa/urls.py
+++ educa/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+path('accounts/login/', auth_views.login, name='login'),
+path('accounts/logout/', auth_views.logout, name='logout'),
+path('admin/', admin.site.urls),
+path('course/', include('courses.urls')),
 ]
+
