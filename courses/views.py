4b6fcd98d4bc9a3de902a57848e91f014c983712
ohrabo01 <ohrabo01@mail.bbk.ac.uk>
2018-07-24 08:58:03 +0100

    code editing modules works

--- courses/views.py
+++ courses/views.py
@@ -68,12 +68,17 @@ class CourseDeleteView(PermissionRequiredMixin, OwnerCourseMixin, DeleteView):
 class CourseModuleUpdateView(TemplateResponseMixin, View):
     template_name = 'courses/manage/module/formset.html'
     course = None
+
     def get_formset(self, data=None):
-        return ModuleFormSet(instance=self.course,data=data)
+        return ModuleFormSet(instance=self.course,
+                             data=data)
 
     def dispatch(self, request, pk):
-        self.course = get_object_or_404(Course,id=pk, owner=request.user)
-        return super(CourseModuleUpdateView, self).dispatch(request, pk)
+        self.course = get_object_or_404(Course,
+                                        id=pk,
+                                        owner=request.user)
+        return super(CourseModuleUpdateView,
+                     self).dispatch(request, pk)
 
     def get(self, request, *args, **kwargs):
         formset = self.get_formset()
